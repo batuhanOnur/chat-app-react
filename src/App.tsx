28a7c2402ac738657cec60a03b8bf930094e762a
batuhanOnur <batuhanonur29@gmail.com>
2022-09-12 21:22:15 +0300

    Creating LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<RegisterPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
         <Route
         path="conversations"
         element={
